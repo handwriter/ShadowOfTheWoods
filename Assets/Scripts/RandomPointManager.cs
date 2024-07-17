2ff0936dd51d51aa70181a9203fe5df063ff36de
MaxPopoff <popov.m@softintermob.com>
2024-07-17 20:05:56 +0300

    Добавлена рандомная генерация стартовой позиции

--- Assets/Scripts/RandomPointManager.cs
+++ Assets/Scripts/RandomPointManager.cs
@@ -12,11 +12,13 @@ public class RandomPointManager : MonoBehaviour
         public string key;
         public int defaultPointIndex;
         public GameObject[] points;
-        public GameObject defaultPoint => points[defaultPointIndex];
+        public GameObject defaultPoint => defaultPointIndex < 0 ? null : points[defaultPointIndex];
+        public GameObject placingObject;
     }
 
     public PointGroup[] Groups;
     private Dictionary<string, PointGroup> _groups = new Dictionary<string, PointGroup>();
+    public bool IsFirstPlay;
 
     private void Awake()
     {
@@ -25,10 +27,21 @@ public class RandomPointManager : MonoBehaviour
 
     public Vector3 GetRandomPoint(string key, bool useDefaultValue = false)
     {
-        if (useDefaultValue)
+        if (useDefaultValue && _groups[key].defaultPointIndex != -1)
         {
             return _groups[key].defaultPoint.transform.position;
         }
         return _groups[key].points[UnityEngine.Random.Range(0, _groups[key].points.Length)].transform.position;
+    }
+
+    private void Start()
+    {
+        foreach (PointGroup group in Groups)
+        {
+            if (group.placingObject)
+            {
+                group.placingObject.transform.position = GetRandomPoint(group.key, IsFirstPlay);
+            }
+        }
     }
 }

3310811650909e945edfabd9e2bfe7533539cae9
BnWGitProject <IliaSamohin@mail.ru>
2024-08-19 21:41:36 +0300

    ShadoOfTheWoods_0.25_2

---
+++ Assets/Locations/Cemetery HDRP/Scripts/MovingLights/Node.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public float gizmos_Radius = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, gizmos_Radius);
+    }
+}
+
+
